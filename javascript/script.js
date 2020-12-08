e679f5d06039f3cb53e2c9dd12d7a6a8d5e7bc9c
tteets09 <tonyteets123@gmail.com>
2020-12-08 17:36:43 -0500

    removed sections 2-4 on main index

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,7 @@
 
-const continueLink = document.getElementById("continueLink");
-const one = document.getElementById("one");
 disableScroll();
 
 //After click on continue button. Remove section one.
-continueLink.addEventListener('click', () => {
-    setTimeout(function() {
-        one.remove();
-        enableScroll()
-    }, 800);
-});
 
 //Allows links in same page to scroll smoothly to them.
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
