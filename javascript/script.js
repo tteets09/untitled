e54ce13e5c45c575b22be6ea0e1d9a834e34c314
tteets09 <tonyteets123@gmail.com>
2020-12-07 20:17:21 -0500

    Added smooth scrolling with in page links

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+
+//Allows links in same page to scroll smoothly to them.
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
