a292150cf9f999d99c7b85e56239fed3873f972e
tteets09 <tonyteets123@gmail.com>
2020-12-07 20:44:32 -0500

    Added scroll disable and enable. When continue button is clicked it will remove the first section of the page containing the continue button and enable scrolling

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,15 @@
 
+const continueLink = document.getElementById("continueLink");
+const one = document.getElementById("one");
+disableScroll();
+
+//After click on continue button. Remove section one.
+continueLink.addEventListener('click', () => {
+    setTimeout(function() {
+        one.remove();
+    }, 1000);
+});
+
 //Allows links in same page to scroll smoothly to them.
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
@@ -9,3 +20,12 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+//Disabling Scrolling
+function disableScroll() { 
+    document.body.classList.add("stop-scrolling"); 
+} 
+  
+function enableScroll() { 
+    document.body.classList.remove("stop-scrolling"); 
+} 
